65de45739524e4a4b7d011db3952e96106a2125a
nikolashchuk <nikolashchuk@gmail.com>
2022-10-22 15:59:38 +0300

    ice-cream

---
+++ src/scripts/mobile-menu.js
@@ -0,0 +1,38 @@
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+
+  // При клике на ссылку навигации в мобильном меню закрываем мобильное меню
+  const closeMobileBtn = document.querySelectorAll('.menu-nav__link');
+  closeMobileBtn.forEach(function (item) {
+    item.addEventListener('click', function (e) {
+      const menu = document.querySelector('#mobile-menu');
+      menu.classList.remove('is-open');
+      bodyScrollLock.enableBodyScroll(document.body);
+    });
+  });
+})();
---
+++ src/scripts/slick.js
@@ -0,0 +1,20 @@
+$('.gallery-slider').slick({
+  autoplay: true,
+  autoplaySpeed: 3000, // 3 seconds
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+});
+
+
+$('.one-time').slick({
+  autoplay: true,
+  autoplaySpeed: 5000, // 5 seconds
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
+});
